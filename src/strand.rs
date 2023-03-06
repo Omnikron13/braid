671e7bae5ee3efda23a0b4b4face9642d41abca4
Joey Sabey <joey.sabey@gmx.com>
2023-03-06 16:13:58 +0000

    add skip_iter() - logic for iterating all leaves which overlap given char range. (this is likely kinda useless without some callback functionality to allow trimming excess characters from the head & tail of the first & last nodes

--- src/strand.rs
+++ src/strand.rs
@@ -156,6 +156,38 @@ impl<'a> Strand<'a> {
          },
       }
    }
+
+
+   // Return an iterator over all leaf nodes which overlap a given char range
+   fn skip_iter(&'a self, mut y: usize, mut z: usize) -> Box<dyn Iterator<Item = Rc<LeafNode>> + 'a> {
+      // SHort-circuit flag if the end index has already been filtered
+      let mut end = false;
+
+      Box::new(self.leaf_iter().filter(move |x| {
+         // Adjusted start index, end index, current strand length & string value
+         println!("# y: {y}, z: {z}, [{}]< {} >", x.length, x.value);
+
+         // Short circuit out strands past the strand containing the end index
+         if end { return false };
+
+         // If the current strand does not contain the start index, filter it out abd adjust the index
+         if y >= x.length {
+            y -= x.length;
+            return false;
+         }
+
+         // If the current strand does not contain the end index, filter it out and adjust the index
+         if z + y >= x.length {
+            z -= x.length;
+            return true;
+         }
+
+         // If the logic reaches this point, the end index is within the current strand.
+         // The next iteration will read the new end flag and filter out the rest of the strands.
+         end = true;
+         return true;
+      }))
+   }
 }
 
 
@@ -410,6 +442,28 @@ mod tests {
       println!("{st:#?}");
       let iter = st.leaf_iter();
       assert_eq!(iter.map(|leaf| leaf.as_ref().value).collect::<String>(), "[foo:bar]");
+   }
+
+   // Test skip_iter(), which iterates all leaves which contain any part of a given char range
+   #[test]
+   fn test_skip_iterator() {
+      let st = Strand::new_branch(
+         Strand::new_branch(
+            Strand::new_leaf("[[["),
+            Strand::new_leaf("=foo"),
+         ),
+         Strand::new_branch(
+            Strand::new_leaf(":~:"),
+            Strand::new_branch(
+               Strand::new_leaf("bar="),
+               Strand::new_leaf("]]]"),
+            ),
+         ),
+      );
+      println!("full strand: {st:#?}");
+      println!("as string: {}", st.leaf_iter().map(|leaf| leaf.as_ref().value).collect::<String>());
+      let iter = st.skip_iter(4, 9);
+      assert_eq!(iter.map(|leaf| leaf.as_ref().value).collect::<String>(), "=foo:~:bar=");
    }
 
 
