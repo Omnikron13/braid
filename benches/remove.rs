6f44d0b713ad8b90928c7012eea3b254626e622b
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 18:54:42 +0100

    condensed benches/remove.rs

--- benches/remove.rs
+++ benches/remove.rs
@@ -1,15 +1,13 @@
 // These benchmarks were lifted from ropey, to compare and sanity check our performance.
 // Consequently, they should be considered as licensed under the MIT license.
-
 extern crate criterion;
 extern crate rand;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use rand::random;
 use braid::strand::Strand;
 
 const TEXT: &str = include_str!("data/large");
-const TEXT_SMALL: &str = include_str!("data/small");
 
 fn mul_string_length(text: &str, n: usize) -> String {
    let mut mtext = String::new();
@@ -19,209 +17,62 @@ fn mul_string_length(text: &str, n: usize) -> String {
    mtext
 }
 
-
-const LEN_MUL_SMALL: usize = 1;
-
-fn remove_small(c: &mut Criterion) {
-   let mut group = c.benchmark_group("remove_small");
-
-   group.bench_function("random", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_SMALL);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = random::<usize>() % len;
-         rope = rope.remove(start, 1);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("start", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_SMALL);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let _len = rope.length();
-         let start = 0;
-         rope = rope.remove(start, 1);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("middle", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_SMALL);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len / 2;
-         rope = rope.remove(start, 1);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("end", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_SMALL);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len - 1;
-         rope = rope.remove(start, 1);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
+fn remove(c: &mut Criterion) {
+   let mut g = c.benchmark_group("remove");
+   for (name, data, mult) in [
+      ("small", 1, 1),
+      ("medium", 15, 1),
+      ("large", 1522, 4),
+   ].iter() {
+      g.bench_with_input(BenchmarkId::new("random", name), &data, |b, data| {
+         let text = mul_string_length(TEXT, *mult as usize);
+         let mut rope = Strand::new_leaf(&text);
+         b.iter(|| {
+            let len = rope.length();
+            let start = random::<usize>() % (len - **data as usize);
+            rope = rope.remove(start, **data as usize);
+            if rope.length() <= TEXT.len() / 2 {
+               rope = Strand::new_leaf(&text);
+            }
+         });
+      });
+      g.bench_with_input(BenchmarkId::new("start", name), &data, |b, data| {
+         let text = mul_string_length(TEXT, *mult as usize);
+         let mut rope = Strand::new_leaf(&text);
+         b.iter(|| {
+            let start = 0;
+            rope = rope.remove(start, **data as usize);
+            if rope.length() <= TEXT.len() / 2 {
+               rope = Strand::new_leaf(&text);
+            }
+         });
+      });
+      g.bench_with_input(BenchmarkId::new("middle", name), &data, |b, data| {
+         let text = mul_string_length(TEXT, *mult as usize);
+         let mut rope = Strand::new_leaf(&text);
+         b.iter(|| {
+            let len = rope.length();
+            let start = len / 2;
+            rope = rope.remove(start, **data as usize);
+            if rope.length() <= TEXT.len() / 2 {
+               rope = Strand::new_leaf(&text);
+            }
+         });
+      });
+      g.bench_with_input(BenchmarkId::new("end", name), &data, |b, data| {
+         let text = mul_string_length(TEXT, *mult as usize);
+         let mut rope = Strand::new_leaf(&text);
+         b.iter(|| {
+            let len = rope.length();
+            let start = len - **data as usize;
+            rope = rope.remove(start, **data as usize);
+            if rope.length() <= TEXT.len() / 2 {
+               rope = Strand::new_leaf(&text);
+            }
+         });
+      });
+   }
 }
-
-
-const LEN_MUL_MEDIUM: usize = 1;
-
-fn remove_medium(c: &mut Criterion) {
-   const MED_LEN: usize = 15;
-   let mut group = c.benchmark_group("remove_medium");
-
-   group.bench_function("random", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_MEDIUM);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = random::<usize>() % (len - (MED_LEN-1));
-         rope = rope.remove(start, MED_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("start", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_MEDIUM);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let _len = rope.length();
-         let start = 0;
-         rope = rope.remove(start, MED_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("middle", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_MEDIUM);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len / 2;
-         rope = rope.remove(start, MED_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("end", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_MEDIUM);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len - MED_LEN;
-         rope = rope.remove(start, MED_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-}
-
-
-const LEN_MUL_LARGE: usize = 4;
-
-fn remove_large(c: &mut Criterion) {
-   const LARGE_LEN: usize = TEXT_SMALL.len();
-   let mut group = c.benchmark_group("remove_large");
-
-   group.bench_function("random", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_LARGE);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = random::<usize>() % (len - (LARGE_LEN-1));
-         rope = rope.remove(start, LARGE_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("start", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_LARGE);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let _len = rope.length();
-         let start = 0;
-         rope = rope.remove(start, LARGE_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("middle", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_LARGE);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len / 2;
-         rope = rope.remove(start, LARGE_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-
-   group.bench_function("end", |bench| {
-      let text = mul_string_length(TEXT, LEN_MUL_LARGE);
-      let mut rope = Strand::new_leaf(&text);
-
-      bench.iter(|| {
-         let len = rope.length();
-         let start = len - LARGE_LEN;
-         rope = rope.remove(start, LARGE_LEN);
-
-         if rope.length() <= TEXT.len() / 2 {
-            rope = Strand::new_leaf(&text);
-         }
-      })
-   });
-}
-
 
 fn remove_initial_after_clone(c: &mut Criterion) {
    c.bench_function("remove_initial_after_clone", |bench| {
@@ -243,9 +94,7 @@ fn remove_initial_after_clone(c: &mut Criterion) {
 
 criterion_group!(
    benches,
-   remove_small,
-   remove_medium,
-   remove_large,
+   remove,
    remove_initial_after_clone
 );
 criterion_main!(benches);
