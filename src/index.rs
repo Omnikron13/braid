692dd99cbaec99d2418e3faddba7f0334110b9eb
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 14:26:50 +0100

    remove comment

--- src/index.rs
+++ src/index.rs
@@ -1,6 +1,3 @@
-// I'd call this delightful dance an anti-pattern, but it bafflingly seems to be
-// the accepted way to deal with the artifically long, semantically meaningless,
-// inelegant stutterings that rust insists on baking into module paths...
 pub mod char_width;
 pub mod newline;
 
