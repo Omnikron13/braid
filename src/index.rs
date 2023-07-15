59270d72d092ac133a8d3d744cddcd42e9b7fc92
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 01:49:25 +0100

    add IndexBuilder trait

--- src/index.rs
+++ src/index.rs
@@ -8,3 +8,8 @@ pub use char_width::CharWidthBuilder;
 pub trait Index {
    fn split(&self, r: std::ops::Range<usize>) -> (Option<Self>, Option<Self>) where Self: Sized;
 }
+
+trait IndexBuilder {
+   fn push(&mut self, c: char);
+   fn freeze(self) -> dyn Index;
+}
