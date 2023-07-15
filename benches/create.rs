ed32b5be5b7ce01c41c421bdcccf9d4322bba623
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 18:09:49 +0100

    rework benches/create.rs

--- benches/create.rs
+++ benches/create.rs
@@ -3,43 +3,31 @@
 
 extern crate criterion;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::black_box;
 use braid::strand::Strand;
-
-const TEXT_SMALL: &str = include_str!("data/small");
-const TEXT_MEDIUM: &str = include_str!("data/medium");
-const TEXT_LARGE: &str = include_str!("data/large");
-const TEXT_LF: &str = include_str!("data/lf");
 
 
 fn from_str(c: &mut Criterion) {
-   let mut group = c.benchmark_group("from_str");
+   let mut g = c.benchmark_group("from_str");
+   g.sample_size(1000);
 
-   group.bench_function("small", |bench| {
-      bench.iter(|| {
-         Strand::new_leaf(black_box(TEXT_SMALL));
-      })
-   });
-
-   group.bench_function("medium", |bench| {
-      bench.iter(|| {
-         Strand::new_leaf(black_box(TEXT_MEDIUM));
-      })
-   });
-
-   group.bench_function("large", |bench| {
-      bench.iter(|| {
-         Strand::new_leaf(black_box(TEXT_LARGE));
-      })
-   });
-
-   group.bench_function("linefeeds", |bench| {
-      bench.iter(|| {
-         Strand::new_leaf(black_box(TEXT_LF));
-      })
-   });
+   for (name, data) in [
+      ("small", include_str!("data/small")),
+      ("medium", include_str!("data/medium")),
+      ("large", include_str!("data/large")),
+      ("linefeeds", include_str!("data/lf")),
+   ].iter() {
+      g.bench_with_input(BenchmarkId::new("", name), &data, |b, data| {
+         b.iter(|| {
+            Strand::new_leaf(data);
+         });
+      });
+   }
 }
 
+
+const TEXT_LARGE: &str = include_str!("data/large");
 
 fn rope_clone(c: &mut Criterion) {
    let rope = Strand::new_leaf(TEXT_LARGE);
@@ -50,5 +38,9 @@ fn rope_clone(c: &mut Criterion) {
    });
 }
 
-criterion_group!(benches, from_str, rope_clone,);
+criterion_group!(
+   benches,
+   from_str,
+   rope_clone,
+);
 criterion_main!(benches);
