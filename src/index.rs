3832c60ea4d7c440541b7c7069681bf9601a9fcb
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 14:27:15 +0100

    add copyright

--- src/index.rs
+++ src/index.rs
@@ -1,3 +1,8 @@
+// Copyright 2023 Joey Sabey <joey.sabey@gmx.com>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
 pub mod char_width;
 pub mod newline;
 
