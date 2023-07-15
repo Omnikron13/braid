9a2946e99ca8fb23eef458f6727956ca9d12ab55
Joey Sabey <joey.sabey@gmx.com>
2023-07-15 16:58:21 +0100

    remove newline

--- benches/index.rs
+++ benches/index.rs
@@ -46,7 +46,6 @@ fn count(c: &mut Criterion) {
 fn byte_index(c: &mut Criterion) {
    let mut g = c.benchmark_group("byte_index");
    g.sample_size(1000);
-
    for name in [
       "tiny",
       "small",
